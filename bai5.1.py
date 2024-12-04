909a8db52ada763b613fd9559f73e8f8af47485d
ngomanhnguyen <ngomanhnguyen610@gmail.com>
2024-12-04 21:03:24 +0700

    Add files via upload

---
+++ bai5.1.py
@@ -0,0 +1,13 @@
+print("Ngo Manh Nguyen MSSV: 235752021610058")
+# Import module mymath
+import mymath
+
+# Sử dụng các hàm trong module
+a = 6
+b = 8
+
+print(f"{a} + {b} = {mymath.add(a, b)}")
+print(f"{a} - {b} = {mymath.subtract(a, b)}")
+print(f"{a} * {b} = {mymath.multiply(a, b)}")
+print(f"{a} / {b} = {mymath.divide(a, b)}")
+print(f"{a} ^ {b} = {mymath.power(a, b)}")
